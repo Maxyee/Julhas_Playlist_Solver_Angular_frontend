95b36550e18b00d5c1f4d6826fde46619a297c1c
maxyee <eyaminkhan00@gmail.com>
2019-04-18 18:02:05 +0600

    Added some component and wildcard route , default route and routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent} from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'', component:LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component:HomeComponent },
+  { path: 'login', component:LoginComponent },
+  { path: '**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [RegisterComponent,HomeComponent,LoginComponent,PageNotFoundComponent]
